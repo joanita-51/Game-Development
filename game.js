228b0e5156c72ea8263931cb6550bc1f81d047b8
joanita-51 <82649346+joanita-51@users.noreply.github.com>
2023-07-18 05:42:17 +0000

    showing the score and the lives of the player

--- game.js
+++ game.js
@@ -3,7 +3,6 @@ canvas.width = 800;
 canvas.height = 400;
 paper.setup(canvas);
 
-
 //Game variables
 let score = 0;
 let lives = 3;
@@ -26,10 +25,12 @@ const blueEnemy = new paper.Path.Rectangle({
 // It is easy to assign an anonymous function to another variable
 paper.view.onKeyDown = function (event) {
   if (event.key === "left") {
-    if (player.bounds.left > 0) { //creating a left boundary
+    if (player.bounds.left > 0) {
+      //creating a left boundary
       player.position.x -= 10;
     }
-  } else if (event.key === "right") { //creating a right boundary
+  } else if (event.key === "right") {
+    //creating a right boundary
     if (player.bounds.right < paper.view.size.width) {
       player.position.x += 10;
     }
@@ -37,24 +38,53 @@ paper.view.onKeyDown = function (event) {
 };
 
 //Detect collisions
-function detectCollisions(){
-  if(player.intersects(blueEnemy)){
-    lives --
-    if(lives == 0){
+function detectCollisions() {
+  if (player.intersects(blueEnemy)) {
+    lives--;
+    if (lives == 0) {
       gameOver = true;
-    }
-  } else {
-    blueEnemy.position.y = 1
+    }else {
+    blueEnemy.position.y = 1; //The position from which the box falls
   }
+  } 
 }
 
-function update(){
-  if(!gameover){
-    blueEnemy.position.y += 5;
-    if(blueEnemy.postion.y > paper.view.size.height){
-      score ++
-      blueEnemy.position.y =1
+function update() {
+  if (!gameOver) {
+    blueEnemy.position.y += 3; //The speed at which the box is falling
+    if (blueEnemy.position.y > paper.view.size.height) {
+      score++;
+      blueEnemy.position.y = 1;
     }
+    detectCollisions();
   }
-  detectCollisions();
+
+  //clear canvas
+  paper.project.activeLayer.removeChildren();
+
+  //Add player
+  paper.project.activeLayer.addChild(player);
+
+  //Add enemy
+  paper.project.activeLayer.addChild(blueEnemy);
+
+  //Score
+  const scoreText = new paper.PointText({
+    point: [10,20],
+    content: "Score: " + score,
+    fillColor: "black",
+    fontSize:16,
+  })
+
+    //Lives
+  const livesText = new paper.PointText({
+    point: [10,60],
+    content: "Lives: " + lives,
+    fillColor: "black",
+    fontSize:16,
+  })
 }
+
+paper.view.onFrame = function (event) {
+  update();
+};
